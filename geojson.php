4ffc86779818613a1c7a04fab82e968efe9783f6
Ken Irwin <kirwin@wittenberg.edu>
2018-06-14 13:40:48 -0700

    geojson.php outpus some geojson based on a person variable

---
+++ geojson.php
@@ -0,0 +1,26 @@
+<?
+require('GeoJsonCollect.class.php');
+$coll = new GeoJsonCollect();
+$barcelona = array("name"=>"Barcelona",
+		   "lat"=>41.3851,
+		   "long"=>2.17434,
+		   );
+$madrid = array ("name" => "Madrid",
+		 "lat"=>40.415363,
+		 "long"=>-3.707398);
+$toledo = array ("name" => "Toledo",
+		 "lat"=>39.8628,
+		 "long"=>-4.0273,);
+$people=array(
+	      'ken'=> array('toledo','madrid'),
+	      'alej' => array('madrid','barcelona')
+	      );
+
+$p = $_REQUEST['person'];
+foreach ($people[$p] as $city) {
+  $temp = $coll->createPoint($$city);
+  $coll->addFeature($temp);
+}
+print($coll->getJson());
+
+?>
