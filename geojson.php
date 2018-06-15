018ee63c023c48d4aba70c4202ee1fdfb744e64a
Ken Irwin <kirwin@wittenberg.edu>
2018-06-15 07:41:26 -0700

    added access control header to geojson.php + additional travelers/dest

--- geojson.php
+++ geojson.php
@@ -1,4 +1,5 @@
 <?
+header('Access-Control-Allow-Origin: *');
 require('GeoJsonCollect.class.php');
 $coll = new GeoJsonCollect();
 $barcelona = array("name"=>"Barcelona",
@@ -11,9 +12,24 @@ $madrid = array ("name" => "Madrid",
 $toledo = array ("name" => "Toledo",
 		 "lat"=>39.8628,
 		 "long"=>-4.0273,);
+$valladolid = array ("name" => "Valladolid",
+		     "lat" => 41.6523,
+		     "long" => -41.6523);
+$caceres = array ("name" => "Caceres",
+		     "lat" => 39.4753,
+		     "long" => -6.3724);
+$cordoba  = array ("name" => "Cordoba",
+		     "lat" => 37.8882,
+		     "long" => -4.7794);
+$zaragoza  = array ("name" => "Zaragoza",
+		     "lat" => 41.6488,
+		     "long" => -0.8891);
 $people=array(
 	      'ken'=> array('toledo','madrid'),
-	      'alej' => array('madrid','barcelona')
+	      'alej' => array('madrid','barcelona'),
+	      'betsy' => array('cordoba','valladolid','caceres'),
+	      'pat' => array('cordoba','toledo'),
+	      'kate' => array('madrid','zaragoza'),
 	      );
 
 $p = $_REQUEST['person'];
